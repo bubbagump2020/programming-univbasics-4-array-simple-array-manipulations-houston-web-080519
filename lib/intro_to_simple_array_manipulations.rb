7c4df294aa422de71b95e12a4ec0f7d2855554e3
Kevin Bagnall <bubbagump2020@gmail.com>
2019-07-17 19:27:18 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -37,14 +37,14 @@ def using_uniq(array)
   p array.uniq
 end
 
-def using_flatten()
+def using_flatten(array)
   p array.flatten
 end
 
-def using_delete()
+def using_delete(array, element)
   
 end
 
-def using_delete_at()
+def using_delete_at(array, element)
   
 end
